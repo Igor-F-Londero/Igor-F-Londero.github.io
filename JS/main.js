ecbe24b172a776244f6d152e7e1e288f90ff31c5
Igor Londero <igorflondero@gmail.com>
2025-07-21 19:13:22 -0300

    form alt

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,11 @@
-function{
-    document.get
+const botao = document.getElementById('toggle-tema')
 
-}
+botao.addEventListener('click', () => {
+  document.body.classList.toggle('light-mode')
+
+  if (document.body.classList.contains('light-mode')) {
+    botao.textContent = 'Modo Escuro'
+  } else {
+    botao.textContent = 'Modo Claro'
+  }
+})
