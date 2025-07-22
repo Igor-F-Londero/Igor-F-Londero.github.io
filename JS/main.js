53a0baa01b653917afd55ffc263bbf3d00772fc5
Igor Londero <igorflondero@gmail.com>
2025-07-22 14:47:26 -0300

    .

--- JS/main.js
+++ JS/main.js
@@ -16,7 +16,7 @@ function atualizaRelogio(){
 
   var agora = new Date();
   
-  var hora = agora.getHours().toString().padStart(2,'0');
+  var hora = agora.getHours().toString().padStart(2,'0');/* padStart() preenche uma string com caracteres extras no início, até ela atingir um tamanho mínimo. */
   var minuto = agora.getMinutes().toString().padStart(2,'0');
   var segundo = agora.getSeconds().toString().padStart(2,'0');
 
