2c5fa9fa2e91d4faa6ce81c5edc34a2fdc27269f
Igor Londero <igorflondero@gmail.com>
2025-07-22 14:41:25 -0300

    relogioEmodo-claro

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,33 @@
-const botao = document.getElementById('toggle-tema')
+function temaclaro(){
+  var botao = document.getElementById('temaclaro');
 
-botao.addEventListener('click', () => {
-  document.body.classList.toggle('light-mode')
+  botao.addEventListener('click', () => {
+    document.body.classList.toggle('temaClaro');
+    if (document.body.classList.contains('temaClaro')){
+    botao.textContent = 'apagar luzes';
+    } else{
+      botao.textContent = 'ligar luzes';
+    }
+  })
+}
+temaclaro();
 
-  if (document.body.classList.contains('light-mode')) {
-    botao.textContent = 'Modo Escuro'
-  } else {
-    botao.textContent = 'Modo Claro'
+function atualizaRelogio(){
+
+  var agora = new Date();
+  
+  var hora = agora.getHours().toString().padStart(2,'0');
+  var minuto = agora.getMinutes().toString().padStart(2,'0');
+  var segundo = agora.getSeconds().toString().padStart(2,'0');
+
+  var horaAtual = `${hora}:${minuto}:${segundo}`;
+
+  let relogio = document.getElementById('relogio');
+  if(relogio){
+    relogio.textContent = horaAtual;
   }
-})
+
+}
+
+atualizaRelogio();
+setInterval(atualizaRelogio, 1000)
